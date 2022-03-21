dcd8b882f2d23c7f39e61ada98601a9551a869bc
shreyash <iamshreyash.in@gmail.com>
2022-03-21 20:41:24 +0530

    Dark Mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,3 @@
-/*==================== PROGRESS BAR ====================*/
-//Source: https://alligator.io/js/progress-bar-javascript-css-variables/
-var h = document.documentElement,
-    b = document.body,
-    st = 'scrollTop',
-    sh = 'scrollHeight',
-    progress = document.querySelector('#progress'),
-    scroll;
-var scrollpos = window.scrollY;
-var header = document.getElementById("header");
-var navcontent = document.getElementById("nav-content");
-
-document.addEventListener('scroll', function() {
-
-    /*Refresh scroll % width*/
-    scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-    progress.style.setProperty('--scroll', scroll + '%');
-
-    /*Apply classes for slide in bar*/
-    scrollpos = window.scrollY;
-    if (scrollpos > 2) {
-        header.classList.add("bg-white");
-        header.classList.add("shadow");
-        navcontent.classList.remove("bg-gray-100");
-        navcontent.classList.add("bg-white");
-    } else {
-        header.classList.remove("bg-white");
-        header.classList.remove("shadow");
-        navcontent.classList.remove("bg-white");
-        navcontent.classList.add("bg-gray-100");
-    }
-});
-/*==================== toggle the menu ====================*/
 /*==================== toggle the menu ====================*/
 const button = document.querySelector('#menu-button');
 const menu = document.querySelector('#menu');
@@ -55,4 +22,71 @@ var typed = new Typed(".auto-input", {
         cursorChar: '|',
         loopCount: Infinity
     })
-    /*==================== SWIPER JS ====================*/
+
+    var typed = new Typed(".auto-input", {
+        strings: [" ", "UI/UX Designer", "React", "Python", "JavaScript"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
+        loopCount: Infinity,
+      });
+      
+      kofiWidgetOverlay.draw("ishreyash", {
+        type: "floating-chat",
+        "floating-chat.donateButton.text": "Support me",
+        "floating-chat.donateButton.background-color": "#323842",
+        "floating-chat.donateButton.text-color": "#fff",
+      });
+      
+
+    /*==================== Dark Mode ====================*/
+      
+      const switchToggle = document.querySelector('#switch-toggle');
+      const html = document.querySelector('html');
+      let isDarkmode = false
+      const localDarkmode = JSON.parse(localStorage.getItem('isDarkmode'))
+      
+      const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+      </svg>`
+      
+      const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+      </svg>`
+      
+      
+      // Jika ada isDarkmode di localstorage 
+      if (localDarkmode) {
+        isDarkmode = localDarkmode
+        html.classList.add('dark')
+      } else {
+        html.classList.remove('dark')
+      }
+      
+      function toggleTheme (){
+        isDarkmode = !isDarkmode
+        localStorage.setItem('isDarkmode', isDarkmode)
+        switchTheme()
+      }
+      
+      function switchTheme (){
+        if (isDarkmode) {
+          html.classList.add('dark')
+          switchToggle.classList.remove('-translate-x-2')
+          switchToggle.classList.add('translate-x-full')
+          setTimeout(() => {
+            switchToggle.innerHTML = darkIcon
+          }, 250);
+        } else {
+          html.classList.remove('dark')
+          switchToggle.classList.add('-translate-x-2')
+          switchToggle.classList.remove('translate-x-full')
+          setTimeout(() => {
+            switchToggle.innerHTML = lightIcon
+          }, 250);
+        }
+      }
+      
+      switchTheme()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./index.html", "posts/**/*.html"],
+  darkMode: 'class',
   theme: {
     extend: {},
     fontFamily: {
