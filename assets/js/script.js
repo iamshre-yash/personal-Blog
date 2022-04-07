2ec2a0d7552cf155d41edbe0b99174acb05b3d32
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-04-07 09:55:05 +0530

    Home page Animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,6 @@ const menu = document.querySelector('#menu');
 button.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
-/*==================== TYPE JS ====================*/
-var typed = new Typed(".auto-input", {
-    strings: [
-        ' ',
-        'UI/UX Designer',
-        'React',
-        'Python',
-        'JavaScript'
-    ],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-    showCursor: true,
-    cursorChar: '|',
-    loopCount: Infinity
-})
-
-
-
-kofiWidgetOverlay.draw("ishreyash", {
-    type: "floating-chat",
-    "floating-chat.donateButton.text": "Support me",
-    "floating-chat.donateButton.background-color": "#323842",
-    "floating-chat.donateButton.text-color": "#fff",
-});
-
 
 /*==================== Dark Mode ====================*/
 
@@ -82,3 +56,79 @@ function switchTheme() {
 }
 
 switchTheme()
+
+/*==================== TYPE JS ====================*/
+var typed = new Typed(".auto-input", {
+    strings: [
+        ' ',
+        'UI/UX Designer',
+        'React',
+        'Python',
+        'JavaScript'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    loopCount: Infinity
+})
+
+/*==================== Reveal sections ====================*/
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+
+allSections.forEach(function(section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+
+});
+
+/*==================== Lazy loading images ====================*/
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function() {
+        entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
