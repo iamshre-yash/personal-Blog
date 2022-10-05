5215b043008baa838aef0829941a042fcd6ba244
Shre Yash <iamshreyash.in@gmail.com>
2022-10-05 10:30:59 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ishreyash.com/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(config: { applyBaseStyles: false })],
 });
